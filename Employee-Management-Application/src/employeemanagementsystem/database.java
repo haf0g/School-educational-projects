a549a0eef7dbda0826cc61b7e71b6004ab355faa
Hafid GARHOUM <92740204+haf0g@users.noreply.github.com>
2025-08-23 02:44:09 +0100

    Update database.java

--- Employee-Management-Application/src/employeemanagementsystem/database.java
+++ Employee-Management-Application/src/employeemanagementsystem/database.java
@@ -8,12 +8,6 @@ package employeemanagementsystem;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- *
- * @author MarcoMan
- * Subscribe our Channel --> https://www.youtube.com/channel/UCPgcmw0LXToDn49akUEJBkQ
- * Thanks for the support guys! <3
- */
 public class database {
     
     public static Connection connectDb(){
@@ -22,7 +16,7 @@ public class database {
             
             Class.forName("com.mysql.jdbc.Driver");
             
-            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/employeemanagement", "root", "0661837002");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/employeemanagement", "root", "password");
             return connect;
         }catch(Exception e){e.printStackTrace();}
         return null;
